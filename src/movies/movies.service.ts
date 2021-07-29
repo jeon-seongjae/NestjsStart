9c3982c66d38fcac27e19ec4976c4c0c4f556ae4
sj jeon <sj.jeon@weballin.com>
2021-07-29 17:24:04 +0900

    fix:tues

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { throws } from 'assert';
 import { CreateMovieDto } from './dto/create-movie.dto';
-import { UpdateMovieDto } from './dto/update.movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()  // dependency injection
